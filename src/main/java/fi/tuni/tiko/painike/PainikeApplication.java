055b3389dae8ce4a2a575d7f48c810853706806b
annametsapelto <anna.metsapelto@gmail.com>
2020-02-18 22:25:47 +0200

    Initial commit

---
+++ src/main/java/fi/tuni/tiko/painike/PainikeApplication.java
@@ -0,0 +1,13 @@
+package fi.tuni.tiko.painike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PainikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PainikeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fi/tuni/tiko/painike/PainikeApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.tuni.tiko.painike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PainikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
